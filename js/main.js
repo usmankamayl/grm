3672cfe57a3fbc3348bb7408975a4a689626f705
usmankamayl <usman.kamailaev@yandex.ru>
2022-02-02 17:46:06 +0300

    task-1

---
+++ js/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const modalTitle = document.querySelector('.modal__title');
+const modalForm = document.querySelector('.modal__form');
+const modalCheckbox = document.querySelector('.modal__checkbox');
+const modalInput = document.querySelector('.modal__checkbox + .modal__input');
+
+
+console.log(modalTitle);
+console.log(modalForm);
+console.log(modalCheckbox);
+console.log(modalInput);
+
